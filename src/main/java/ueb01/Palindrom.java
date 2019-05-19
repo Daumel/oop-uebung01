a76704bc32d7c09ba70f5b4f0779ebc019f8433e
Daumel <nikiwilhelm98@googlemail.com>
2019-05-19 11:05:39 +0200

    Methoden wurden implementiert

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -2,8 +2,20 @@ package ueb01;
 
 class Palindrom {
 	static boolean istPalindrom(String s) {
-		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+		CharStack stack = new CharStackImpl();
+
+		s = s.toLowerCase();
+		s = s.replaceAll(" ", "");
+
+		for(char c: s.toCharArray()){
+			stack.push(c);
+		}
+		int a = stack.size();
+		String backwards = "";
+		for(int i = 0; i<a; i++){
+			backwards += stack.pop();
+		}
+		return s.equals(backwards);
 	}
 }
